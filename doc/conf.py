86ac0b8c00fb8f0574fd2a36952d390c7e652083
Rob Bovill <rbovill@lsst.org>
2023-07-28 16:28:32 -0700

    Use ts_pre_commit_conf.

--- doc/conf.py
+++ doc/conf.py
@@ -1,11 +1,8 @@
 """Sphinx configuration file for TSSW package"""
 
-from documenteer.conf.pipelinespkg import *
-
+from documenteer.conf.pipelinespkg import *  # type: ignore # noqa
 
 project = "ts_SalMultiLanguageTests"
-html_theme_options["logotext"] = project  #  type: ignore
+html_theme_options["logotext"] = project  #  type: ignore # noqa
 html_title = project
 html_short_title = project
-
-intersphinx_mapping["ts_xml"] = ("https://ts-xml.lsst.io", None)  #  type: ignore
